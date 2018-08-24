2cb402f7c79d1170f960b44318974f49d1a6c801
Ravi Gavarasana <rgavarasana@evolenthealth.com>
2018-08-23 21:20:26 -0400

    claims transformation

---
+++ ravi.learn.identity.mvc/Services/IProfileService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ravi.learn.identity.mvc.Services
+{
+    public interface IProfileService
+    {
+        Task<UserProfile> GetUserProfileAsync(string userId);
+    }
+
+    public class UserProfile
+    {
+        public UserProfile(string firstName, string lastName, string[] roles)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Roles = roles;
+        }
+
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string[] Roles { get; }
+    }
+}
